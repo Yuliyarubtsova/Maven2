77103e2547eab90c784a8920ca5604ddac929339
Yuliya Rubtsova <yuliya.rubcova89@mail.ru>
2021-01-23 15:46:23 +0800

    fixed #2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
+    long expected = 60;
 
-    service.calculate(amount, registered);
+    long actual = service.calculate(amount, registered);
+    boolean passed = expected == actual;
+    System.out.println(passed);
   }
 }
 
