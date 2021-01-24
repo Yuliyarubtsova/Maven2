47719d93cb4e5c1edc8f9badeb5786bd5edd491d
Yuliya Rubtsova <yuliya.rubcova89@mail.ru>
2021-01-24 18:49:32 +0800

    fixed #3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
 public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
-
     long amount = 1000_60;
     boolean registered = true;
-
     long actual = service.calculate(amount, registered);
-
     System.out.println(actual);
     }
 }
